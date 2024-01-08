9b186681791f62e7de5a66155d18682b8e223483
samir shaikh <samir.s@prismitsystems.com>
2024-01-08 18:00:17 +0530

    ..

--- src/components/DashboardPage/DashboardPage.tsx
+++ src/components/DashboardPage/DashboardPage.tsx
@@ -92,7 +92,7 @@ const DashboardPage = () => {
       // const finalSign = md5Hash.toUpperCase();
 
 
-      const finalUrl = `https://api-sg.aliexpress.com/sync?${JSON.stringify(parameters)}&sign=${finalSign.data}`;
+      const finalUrl = `https://api-sg.aliexpress.com/sync?${parameters}&sign=${finalSign.data}`;
 
       let config: any = {
         method: "post",
